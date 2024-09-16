fb76d664e06f649795f6c904d5c27ba418e1fd38
Morteza <morteza.brothers@gmail.com>
2024-09-16 00:02:14 -0400

    update plotter tpPlot_Inexpensivekmer_nopolish_recall_and_TP.py

--- plot/tpPlot_Inexpensivekmer_nopolish_recall_and_TP.py
+++ plot/tpPlot_Inexpensivekmer_nopolish_recall_and_TP.py
@@ -60,7 +60,8 @@ with open(input_file2, 'r') as file:
         tp_y6.append(float(data[6]))  # Convert y6 value to float
 
 # Create a figure and axes for both plots
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 15))
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(18, 25))
+plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.95, hspace=0.4)
 
 bar_width = 0.15
 
@@ -69,11 +70,11 @@ ax1.plot(x, y1, 's-', label=second_ax_name, color='navy', markersize=10)
 ax1.plot(x, y2, '^-', label=third_ax_name, color='darkseagreen', markersize=10)
 ax1.plot(x, y5, 'v-', label=sixth_ax_name, color='firebrick', markersize=10)
 
-ax1.legend(fontsize=16, loc='lower right')
-ax1.tick_params(axis='both', which='major', labelsize=18)
-ax1.set_ylabel('Recall Rate (%)', fontsize=20)
-ax1.set_xlabel('Minimum Alignment Size (R)', fontsize=20)
-ax1.text(-0.1, 1.1, '(A)', transform=ax1.transAxes, fontsize=24, fontweight='bold', va='top', ha='right', color='blue')
+ax1.legend(fontsize=20, loc='lower right')
+ax1.tick_params(axis='both', which='major', labelsize=20)
+ax1.set_ylabel('Recall Rate (%)', fontsize=24)
+ax1.set_xlabel('Minimum Alignment Size (R)', fontsize=24)
+ax1.text(-0.1, 1.1, '(A)', transform=ax1.transAxes, fontsize=26, fontweight='bold', va='top', ha='right', color='blue')
 
 # Specify the range for the zoomed-in plot
 zoom_start = int(26 - x[0])
@@ -88,40 +89,82 @@ ax2.bar([i + 2 * bar_width for i in x[zoom_start:zoom_end]], y2[zoom_start:zoom_
 ax2.bar([i + 3 * bar_width for i in x[zoom_start:zoom_end]], y4[zoom_start:zoom_end], label=fifth_ax_name, color='darkseagreen', width=bar_width)  # Shift x values for Dataset 4
 ax2.bar([i + 4 * bar_width for i in x[zoom_start:zoom_end]], y5[zoom_start:zoom_end], label=sixth_ax_name, color='firebrick', width=bar_width)  # Shift x values for Dataset 4
 
-ax2.legend(fontsize=16, loc='lower right')
-ax2.tick_params(axis='both', which='major', labelsize=18)
+ax2.legend(fontsize=20, loc='lower right')
+ax2.tick_params(axis='both', which='major', labelsize=20)
 
-# Set y-axis limits to 50-100 for the zoomed-in plot
 ax2.set_ylim(80, 100)
+ax2.set_yticks([80, 85, 90, 95, 100])
 
 # Format the y-axis ticks
 # formatter = ticker.FuncFormatter(lambda x, pos: '0' if x == 0 else '{:.0f}k'.format(x*1e-3))
 # ax2.yaxis.set_major_formatter(formatter)
-ax2.set_ylabel('Recall Rate (%)', fontsize=20)
-ax2.set_xlabel('Minimum Alignment Size (R)', fontsize=20)
+ax2.set_ylabel('Recall Rate (%)', fontsize=24)
+ax2.set_xlabel('Minimum Alignment Size (R)', fontsize=24)
 
-ax2.text(-0.1, 1.1, '(B)', transform=ax2.transAxes, fontsize=24, fontweight='bold', va='top', ha='right', color='blue')
+ax2.text(-0.1, 1.1, '(B)', transform=ax2.transAxes, fontsize=26, fontweight='bold', va='top', ha='right', color='blue')
 
 #ax3
-ax3.bar(x[zoom_start:zoom_end], tp_y1[zoom_start:zoom_end], label=first_ax_name, color='grey', width=bar_width)
+ax3.bar(tp_x[zoom_start:zoom_end], tp_y1[zoom_start:zoom_end], label=first_ax_name, color='grey', width=bar_width)
 ax3.bar([i + bar_width for i in tp_x[zoom_start:zoom_end]], tp_y2[zoom_start:zoom_end], label=second_ax_name, color='navy', width=bar_width)  # Shift x values for Dataset 2
 ax3.bar([i + 2 * bar_width for i in tp_x[zoom_start:zoom_end]], tp_y3[zoom_start:zoom_end], label=fourth_ax_name, color='skyblue', width=bar_width)  # Shift x values for Dataset 3
 ax3.bar([i + 3 * bar_width for i in tp_x[zoom_start:zoom_end]], tp_y4[zoom_start:zoom_end], label=third_ax_name, color='green', width=bar_width)  # Shift x values for Dataset 4
 ax3.bar([i + 4 * bar_width for i in tp_x[zoom_start:zoom_end]], tp_y5[zoom_start:zoom_end], label=fifth_ax_name, color='darkseagreen', width=bar_width)  # Shift x values for Dataset 5
 ax3.bar([i + 5 * bar_width for i in tp_x[zoom_start:zoom_end]], tp_y6[zoom_start:zoom_end], label=sixth_ax_name, color='firebrick', width=bar_width)  # Shift x values for Dataset 5
-# ax3.legend(fontsize=18)
-# ax3.legend(fontsize=18, bbox_to_anchor=(.05, 1.1), loc='upper left', borderaxespad=0., ncol=2)
-ax3.legend(fontsize=16, loc='upper right', ncols=2)
-ax3.tick_params(axis='both', which='major', labelsize=18)
+
+ax3.legend(fontsize=20, loc='lower right', ncols=3)
+ax3.tick_params(axis='both', which='major', labelsize=20)
 
 # Format the y-axis ticks
-formatter = ticker.FuncFormatter(lambda x, pos: '0' if x == 0 else '{:.0f}'.format(x*1e-5))
+formatter = ticker.FuncFormatter(lambda x, pos: '0' if x == 0 else '{:.1f}'.format(x*1e-5))
+
 ax3.yaxis.set_major_formatter(formatter)
 scale_factor = 1e5 
-ax3.set_ylabel(f'No. of TP (×10^{int(np.log10(scale_factor))})',  fontsize=20)
+ax3.set_ylabel(f'No. of TP (×10^{int(np.log10(scale_factor))})',  fontsize=24)
 
-ax3.text(-0.1, 1.1, '(C)', transform=ax3.transAxes, fontsize=24, fontweight='bold', va='top', ha='right', color='blue')
-ax3.set_xlabel('Alignment Size', fontsize=20)
+ax3.text(-0.1, 1.1, '(C)', transform=ax3.transAxes, fontsize=26, fontweight='bold', va='top', ha='right', color='blue')
+ax3.set_xlabel('Alignment Size', fontsize=24)
+
+# ax4
+# Data
+x_values = np.array([132, 131, 130, 129, 128, 127, 126, 125, 124, 123, 122, 121, 120, 119, 
+                     118, 117, 116, 115, 114, 113, 112, 111, 110, 109, 108, 107, 106, 105, 
+                     104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 
+                     88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 
+                     71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 
+                     54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 
+                     37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 
+                     20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 
+                     1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, 
+                     -15, -16, -17, -18])
+
+y_values = np.array([1, 2, 0, 0, 0, 0, 0, 0, 0, 2, 7, 3, 2, 2, 0, 0, 0, 1, 1, 1, 5, 0, 1, 
+                     1, 0, 0, 0, 0, 10, 14, 1, 1, 1, 98, 0, 0, 0, 0, 1, 1, 39, 2, 60, 23, 
+                     0, 0, 3, 5, 28, 4, 8, 14, 6, 25, 2, 1, 1, 1, 1, 1, 7, 8, 12, 14, 32, 
+                     4, 3, 2, 114, 9, 16, 17, 9, 68, 28, 8, 7, 161, 14, 26, 69, 32, 29, 
+                     154, 60, 16, 23, 63, 12, 351, 73, 56, 137, 307, 167, 406, 68, 41, 157, 
+                     229, 184, 286, 658, 369, 365, 359, 579, 1393, 20083, 1729, 1559, 3121, 
+                     2596, 2603, 5222, 4131, 3534, 18389, 9950, 10519, 22773, 16340, 20420, 
+                     47410, 52987, 75389, 757787, 519113, 531663, 2347512, 5060046, 714383, 
+                     36131627, 7754, 9050, 2842, 6259, 449, 2066, 875, 144, 8, 2, 1, 2, 0, 
+                     0, 2, 2, 2, 104])
+
+# Set colors based on x-values
+colors = ['firebrick' if x < 0 else 'navy' if x > 0 else 'grey' for x in x_values]
+
+# Labels
+labels = ['PARMIK IKT0 < Baseline' if x < 0 else 'PARMIK IKT0 > Baseline' if x > 0 else 'PARMIK IKT0 = Baseline' for x in x_values]
+ax4.bar(x_values, y_values, color=colors, label=labels)
+ax4.set_yscale('log') 
+
+# Adding custom legend
+legend_labels = ['PARMIK IKT0 < Baseline', 'PARMIK IKT0 = Baseline', 'PARMIK IKT0 > Baseline']
+legend_colors = ['firebrick', 'grey', 'navy']
+handles = [plt.Rectangle((0, 0), 1, 1, color=color) for color in legend_colors]
+ax4.legend(handles, legend_labels, fontsize=20)
+ax4.tick_params(axis='both', which='major', labelsize=20)
+ax4.set_xlabel('Number of Base Pairs', fontsize=24)
+ax4.set_ylabel('Number of Alignments', fontsize=24)
+ax4.text(-0.1, 1.1, '(D)', transform=ax4.transAxes, fontsize=26, fontweight='bold', va='top', ha='right', color='blue')
 
 
 # Show the plot
